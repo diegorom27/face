37db9356b4c635bacb1815da715d5cf9f36b17c6
diegorom27 <diegorom27@gmail.com>
2024-03-18 11:31:17 -0500

    face app1

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports ={
+    env:{
+        node:true
+    },
+    extends:['standar','prettier'],
+    parseOptions:{
+        ecmaVersion:'latest',
+        sourceType:'module'
+    },
+    "rules":{}
+}
