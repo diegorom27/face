f634ad8d3cd394ac71416b26459736ae90910a46
diegorom27 <diegorom27@gmail.com>
2024-03-18 20:13:08 -0500

    vercel config

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import httpServer from "#config/http.js";
 import "#config/env.js";
 import connectDB from "#config/db.js";
 
-const bootstrap = async () =>{
-    await connectDB(process.env.ATLAS_URI)
-    httpServer.listen(process.env.PORT,()=>{
-        console.log('running on http://localhost:'+process.env.PORT)
-    })
-}
+await connectDB(process.env.ATLAS_URI)
+httpServer.listen(process.env.PORT,()=>{
+    console.log('running on http://localhost:'+process.env.PORT)
+})
 
-bootstrap()
+
+export default httpServer
