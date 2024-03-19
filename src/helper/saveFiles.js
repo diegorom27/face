1a9d9c2f48beb5a684caa9a9e60b6141d80b0752
diegorom27 <diegorom27@gmail.com>
2024-03-18 23:50:27 -0500

    upload dir

--- src/helper/saveFiles.js
+++ src/helper/saveFiles.js
@@ -5,7 +5,7 @@ import {v4 as uuidv4} from 'uuid';
 const storage = multer.diskStorage({ 
     destination: (req, file, cb) => {
         let filePath = path.resolve('./src/public/img/');
-        cb(null, 'public/img/');
+        cb(null, filePath);
     },
     filename: (req, file, cb) => {
         cb(null,uuidv4()+file.originalname);
