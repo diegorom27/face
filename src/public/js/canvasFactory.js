b1a15c9e7d68fd250466ab45c4fbb611c82cb44e
diegorom27 <diegorom27@gmail.com>
2024-03-19 15:55:20 -0500

    Update canvasFactory.js

--- src/public/js/canvasFactory.js
+++ src/public/js/canvasFactory.js
@@ -5,7 +5,7 @@ export async function createCanvas(coors, canvasElement, imgPath = null,imgX=0,i
         if (imgPath) {
             img = new Image();
             const blob = new Blob([imgPath], { type: 'image/png' }); 
-            const imageUrl = URL.createObjectURL(new Uint8Array(blob));
+            const imageUrl = URL.createObjectURL(blob);
             img.src = imageUrl;
         }
         const canvas = new Canvas(coors, canvasElement, img);
