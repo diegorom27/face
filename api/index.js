c15669d5e47b34681a0556329095c18c93c668a4
diegorom27 <diegorom27@gmail.com>
2024-03-18 20:44:07 -0500

    event emitter config

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
+import { EventEmitter } from 'node:events';
 import httpServer from '../src/index.js'
+
+const emitter = new EventEmitter();
+emitter.setMaxListeners(emitter.getMaxListeners() + 1);
 
 export default httpServer
