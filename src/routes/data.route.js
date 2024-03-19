8d17eb95f476a2e5bc7a604d370a7a063532321c
diegorom27 <diegorom27@gmail.com>
2024-03-19 11:46:11 -0500

    Update data.route.js

--- src/routes/data.route.js
+++ src/routes/data.route.js
@@ -17,6 +17,9 @@ router.post('/', async (req, res) => {
     const data = req.body;
     console.log(data);
     console.log('saving')
+    data.frontalImageSrc = Buffer.from(data.frontalImageSrc)
+    data.lateralImageSrc = Buffer.from(data.frontalImageSrc)
+    data.objectiveImageSrc = Buffer.from(data.frontalImageSrc)
     const main = new Main(data);
     console.log(main);
     try {
